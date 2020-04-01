5b4e3b7f62e02949cc6da19b2a226f5c1022d5a2
Fallen_Breath <yqx791125833@gmail.com>
2020-04-02 00:23:52 +0800

    init

---
+++ Calculator.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+
+
+def calc(text):
+	whitelist = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '.', '+', '-', '*', '/', '(', ')', '<', '>', '=']
+	text = text.replace('**', '')  # to avoid eeasee
+	text_clean = ''
+	for c in text:
+		if c in whitelist:
+			text_clean += c
+
+	if len(text_clean) == 0:
+		return None
+	try:
+		return str(eval(text_clean))
+	except Exception as e:
+		return str(e)
+
+
+def work(server, info):
+	if info.content.startswith('=='):
+		result = calc(info.content[2:])
+		if result is not None:
+			server.say(result)
+
+
+# MCDaemon
+def onServerInfo(server, info):
+	if info.isPlayer == 1:
+		work(server, info)
+
+
+# MCDReforged
+def on_info(server, info):
+	if info.is_player:
+		work(server, info)
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+Calculator
+------
+
+A simple in game calculator plugin for [MCDaemon](https://github.com/kafuuchino-desu/MCDaemon) or [MCDReforged](https://github.com/Fallen-Breath/MCDReforged)
+
+## Usage
+
+It will calculate the expression with an extra prefix `==` such as `==1+1`. All unrelated chars will be ignored
+
+Try input `==3*4-(1+3.0)` in game chat and that's it
